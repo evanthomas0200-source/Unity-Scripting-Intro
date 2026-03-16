76023b68cc9ca614f8f630df1da35ef35d8f2595
evanthomas0200-source <evanthomas0200@gmail.com>
2026-03-16 10:36:41 -0400

    Made code

---
+++ Scripting Intro/Assets/MyScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    int my_int = 15;
+    float my_float = 1.5f;
+    char my_char = 'c';
+    string my_string = "C is a letter";
+    Vector2 my_vec2 = new Vector2(12, 8);
+    Vector3 my_vec3 = new Vector3(4.6f, 10.2f, 13.5f);
+    int[] my_array = { 1, 3, 5, 7, 9, 11, 13, 15 };
+    float[] my_array2 = { 2.0f, 4.0f, 6.0f, 8.0f, 10.0f };
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
