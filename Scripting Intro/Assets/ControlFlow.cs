fa4118c79f5cbb5fa537a2c31ab4962d0b4d8259
evanthomas0200-source <evanthomas0200@gmail.com>
2026-03-20 10:35:53 -0400

    added code

--- Scripting Intro/Assets/ControlFlow.cs
+++ Scripting Intro/Assets/ControlFlow.cs
@@ -1,22 +1,34 @@
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ControlFlow : MonoBehaviour
 {
     public float num1 = 10;
-    public float num2 = 20;
+    public float num2 = 2;
+    public float num3 = 5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(num1 + num2);
-        Debug.Log(num1 - num2);
-        Debug.Log(num1 * num2);
-        Debug.Log(num1 / num2);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            num1 += 1;
+            Debug.Log("Number is at: " + num1);
+            if (num1 % 2 == 0)
+            {
+                Debug.Log(num1 + "Is even...");
+            }
+            else
+            {
+                Debug.Log(num1 + "Is odd...");
+            }
+        }
     }
 }
